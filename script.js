3971a66db85a712850fa55b13a5205d73df7ea4c
Zhurkin_Alex <lexus1812141@gmail.com>
2021-06-13 11:02:33 +0300

    js: обернул в одну функцию

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-const slides = document.querySelectorAll('.slide')
-const boxSlide = document.querySelector('.box-slide')
-
-console.log('slide');
-
-slides.forEach(el=>{
-  el.addEventListener('click', ()=>{
-    clearActiveClass()
-    el.classList.add('active')
-  })
-})
-
-function clearActiveClass(){
-  boxSlide.classList.add('width__container')
+function activeClass(activeItem=0){
+  const slides = document.querySelectorAll('.slide')
+  const boxSlide = document.querySelector('.box-slide')
+  
+  slides[activeItem].classList.add('active')
+  
   slides.forEach(el=>{
-    el.classList.remove('active')
+    el.addEventListener('click', ()=>{
+      clearActiveClass()
+      el.classList.add('active')
+    })
   })
+  
+  function clearActiveClass(){
+    boxSlide.classList.add('width__container')
+    slides.forEach(el=>{
+      el.classList.remove('active')
+    })
+  }
 }
+
+activeClass(0)
